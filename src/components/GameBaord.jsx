51b6a2d28211fec39a0d4cc2a4418d51998477e9
MorriganH <morriganhennessy@gmail.com>
2023-07-17 15:37:15 -0400

    tiles can be clicked to add class that changes its colour

--- src/components/GameBaord.jsx
+++ src/components/GameBaord.jsx
@@ -1,17 +1,22 @@
+import "./styles/gameBoard.css";
+
 export default function GameBoard() {
-
-    const elements = []
-    for (let i = 1; i <= 10; i++) {
-      for (let j = 1; j <= 10; j++) {
-        elements.push(`${String.fromCharCode(j + 64)}${i}`);
-      }
+  const elements = [];
+  for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+      elements.push(`${String.fromCharCode(j + 64)}${i}`);
     }
-    const elementsHTML = elements.map(element => <div className="grid-item" key={element}>{element}</div>)
-  
+  }
 
-  return (
-    <div className="gameBoard">
-      {elementsHTML}
+  const handleClick = event => {
+    event.currentTarget.classList.add("hit");
+  };
+
+  const elementsHTML = elements.map(element => (
+    <div className="grid-item" onClick={handleClick} key={element} id={element}>
+      {element}
     </div>
-  );
+  ));
+
+  return <div className="gameBoard">{elementsHTML}</div>;
 }
